3881ca206e653c40850d349dcdb06ee713ac257c
execu <execu@10.0.0.174>
2021-07-06 10:16:45 -0400

    Update

--- src/setup/init.sql
+++ src/setup/init.sql
@@ -5,17 +5,17 @@
 -- ---------------------------------------------------------------------------
 
 INSERT INTO Device(id, name, process) VALUES 
-	("100111", "Arduino Tron IoT", ""), 
-	("100222", "Temperature-Humidity", ""), 
-	("100333", "Door Lock IoT-MCU", ""), 
-	("100444", "Arduino IoT-SensorTag", ""), 
-	("100555", "Arduino Dash Button", ""), 
-	("100666", "Door Open Sensor ESP01", ""), 
-	("100777", "Light Module IoT-MCU", ""), 
-	("100888", "Arduino Tron IoT Display", ""); 
-	("100910", "Jarvis Pi IoT Tron", "com.JarvisPiIoTTron"), 
-	("100920", "EOSpy IoT GPS Position", "com.GPSPositionTron");
-	("100930", "EOSpy TISensorTag GPS Environment", "com.TISensorTagEnvironment");
+	("100111", "Arduino Tron IoT", ""),
+	("100222", "Temperature-Humidity", ""),
+	("100333", "Door Lock IoT-MCU", ""),
+	("100444", "Arduino IoT-SensorTag", ""),
+	("100555", "Arduino Dash Button", ""),
+	("100666", "Door Open Sensor ESP01", ""),
+	("100777", "Light Module IoT-MCU", ""),
+	("100888", "Arduino Tron IoT Display", ""),
+  	("100910", "Jarvis Pi IoT Tron", "com.JarvisPiIoTTron"),
+  	("100920", "EOSpy IoT GPS Position", "com.GPSPositionTron"),
+  	("100930", "EOSpy TISensorTag GPS Environment", "com.TISensorTagEnvironment");
 
 INSERT INTO User(login, name) VALUES
     ("1234", "Steven Woodward"),
