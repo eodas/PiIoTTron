ebb13b312285c8193684c1046f0860cbc94d80c0
execu <execu@10.0.0.174>
2021-07-13 13:45:48 -0400

    Update

--- src/setup/init.sql
+++ src/setup/init.sql
@@ -16,8 +16,8 @@ INSERT INTO Device(id, name, process) VALUES
 	("100888", "Arduino Tron IoT Display", ""),
   	("100910", "Jarvis Pi IoT Tron", "com.JarvisPiIoTTron"),
   	("100920", "EOSpy IoT GPS Position", "com.GPSPositionTron"),
-  	("100930", "IoT-TISensorTag GPS Environment", "com.TISensorTagEnvironment");
-  	("100940", "Arduino Tron M5Stack M5Atom LED Matrix", "com.M5AtomEnvironment");
+  	("100930", "IoT-TISensorTag GPS Environment", "com.TISensorTagEnvironment"),
+  	("100940", "M5Atom LED Arduino Tron M5Stack", "com.M5AtomEnvironment");
 
 INSERT INTO User(login, name) VALUES
     ("1234", "Steven Woodward"),
